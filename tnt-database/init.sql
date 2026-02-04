356c267716dfb31bbc5bfa56647079924d62e5e2
Natalia2026 <natalia.c.weston@gmail.com>
2026-02-04 14:55:26 +0000

    creating tables

--- tnt-database/init.sql
+++ tnt-database/init.sql
@@ -1,6 +1,8 @@
 -- Drop tables if they exist so we can re run this file
 DROP TABLE IF EXISTS articles;
 DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS articleGenres;
+
 
 CREATE TABLE genres(
   genre_id INTEGER GENERATED ALWAYS AS IDENTITY,
@@ -8,17 +10,32 @@ CREATE TABLE genres(
   PRIMARY KEY (genre_id)
 );
 
+CREATE TABLE status(
+  status_id INTEGER GENERATED ALWAYS AS IDENTITY,
+  status_name VARCHAR(100),
+  PRIMARY KEY (status_id)
+);
+
 CREATE TABLE articles(
   article_id INTEGER GENERATED ALWAYS AS IDENTITY,
   article_title VARCHAR(255) NOT NULL,
   article_summary VARCHAR(1000) NOT NULL,
   article_text VARCHAR(10000) NOT NULL,
-  article_published_at INTEGER NOT NULL,
-  article_genre VARCHAR(100),
-  PRIMARY KEY (article_id)
+  artcile_created_at INTEGER NOT NULL,
+  article_published_at INTEGER,
+  article_genre VARCHAR(100) NOT NULL,
+  artcile_time_period INTEGER NOT NULL,
+  article_is_breaking BOOLEAN NOT NULL,
+  article_status_id INTEGER NOT NULL,
+  article_rating INTEGER NOT NULL,
+  article_image_path VARCHAR(500) NOT NULL,
+  PRIMARY KEY (article_id),
+  CONSTRAINT fk_status
+    FOREIGN KEY (article_status_id)
+    REFERENCES status(status_id)
 );
 
-TABLE articleGenres(
+CREATE TABLE articleGenres(
   articleGenre_id INTEGER GENERATED ALWAYS AS IDENTITY,
   article_id INTEGER,
   genre_id INTEGER,
@@ -31,20 +48,7 @@ TABLE articleGenres(
     REFERENCES genres(genre_id)
 );
 
-CREATE TABLE course (
-  course_id     SERIAL PRIMARY KEY,
-  teacher_id    INTEGER REFERENCES teacher(teacher_id),
-  course_name   VARCHAR(255) NOT NULL,
-  course_length VARCHAR(50)
-);
-
 INSERT INTO teacher (first_name, surname) VALUES
   ('Alice', 'Williams'),
   ('Mark', 'Thompson'),
   ('Sara', 'Gatsby');
-
-INSERT INTO course (teacher_id, course_name, course_length) VALUES
-  (1, 'Maths',   '1 term'),
-  (2, 'Music',   '1 term'),
-  (1, 'Physics', '2 terms'),
-  (3, 'History', '1 term');
