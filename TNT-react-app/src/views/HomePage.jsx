fa4120d0f1178bd7a8f41aa3eaf2470b1d4a28e5
chids300 <C.Osuala@corecomtechacademy.co.uk>
2026-02-04 11:34:57 +0000

    removing old way of fetching breaking news article

--- TNT-react-app/src/views/HomePage.jsx
+++ TNT-react-app/src/views/HomePage.jsx
@@ -5,19 +5,9 @@ import { useState, useEffect } from "react"
 
 export function HomePage({ articles }) {
     const [breakingArticles, setBreakingArticles] = useState([])
-    const [topTenArticles, setTopTenArticles] = useState([])
     const [nonBreakingArticles, setNonBreakingArticles] = useState([])
 
-    let breakingNewsArticles = []
-    breakingNewsArticles = articles.filter(article => article.isBreaking === true)
-
-
-
-
     useEffect(() => {
-        // get breaking 
-        // get top 10
-        // get non breaking
         const breaking = articles.filter(article => article.isBreaking)
         const nonBreaking = articles.filter(article => !article.isBreaking)
 
